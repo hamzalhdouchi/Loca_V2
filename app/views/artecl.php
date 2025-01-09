055b0fc65db8b55e0530191b4a2d68b48bf5f3e3
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:48:02 +0000

    added article form handling

--- app/views/artecl.php
+++ app/views/artecl.php
@@ -18,4 +18,14 @@ $articles = $article->getArticleWithTags($limit, $offset);
 $totalArticles = $article->countArticle(); 
 $totalPages = ceil($totalArticles / $itemsPerPage);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_car_theme'])) {
+    $idt = intval($_POST['idT']);
+    $title = htmlspecialchars($_POST['car_title']);
+    $content = htmlspecialchars($_POST['car_content']);
+    $tags = $_POST['car_tags'];
+
+    $image = $_FILES['car_image'];
+    $article->setArticle($idt, $title, $content, $image, $tags);
+}
+
 ?>
