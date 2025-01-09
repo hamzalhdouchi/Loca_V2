0767ec0885db34c1ef4da52f4f7fa5a89d4c22a4
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:08:08 +0000

    Gestion des likes et des commentaires via les requêtes POST

--- app/views/articleditels.php
+++ app/views/articleditels.php
@@ -14,3 +14,26 @@ $comment = $coment->getComment($idA);
 $article = new Article();
 $articles = $article->getArticleSpiciale($idA);
 $SpicialArticle = $articles[0];
+
+$favorite = new favorite();
+$likes = $favorite->getLike($idA,$idU);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['PostComment'])) {
+    $comment = htmlspecialchars($_POST['comment']);
+    $idA = intval($_POST['idA']);
+    $idU = intval($_POST['idU']);
+    $coment->setComment($comment,$idU,$idA);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['likeButton'])) {
+    $like = intval($_POST['like']);
+    $idA = intval($_POST['id_Article']);
+    $idU = intval($_POST['id_user']);
+    $favorite->likeSet($idU,$idA,$like);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['deletLike'])) {
+    $like = intval($_POST['likess']);
+    $idA = intval($_POST['id_Article']);
+    $favorite->deletLike($like,$idA);
+}
