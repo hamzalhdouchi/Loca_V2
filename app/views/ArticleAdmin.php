dea028e85a6132fcfc8c4404f48fb2c27898f6f3
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:57:16 +0000

    Fetch and update article status

--- app/views/ArticleAdmin.php
+++ app/views/ArticleAdmin.php
@@ -2,4 +2,17 @@
 session_start();
 $userName = $_SESSION['user_name'];
 require __DIR__ . "/../controller/Article.php";
+
+<?php
+$themes = new Article();
+
+$result = $themes->getArticle();
+if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['status'])) {
+    $id = $_POST['id'];
+    $status = $_POST['stat'];
+    
+    $themes->Setstatus($status, $id);
+}
+?>
+
 ?>
