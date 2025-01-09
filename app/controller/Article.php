ed2dd019b6ee70b2ac87bab192d163a50a877d67
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:35:15 +0000

    Ajout de la méthode getArticle pour récupérer tous les articles

--- app/controller/Article.php
+++ app/controller/Article.php
@@ -6,4 +6,13 @@ class Article {
         $db = new Database();
         $this->connect = $db->getdatabase();
     }
+
+    public function getArticle() {
+        $sql = "SELECT * FROM article";
+        $stmt = $this->connect->prepare($sql);
+        $stmt->execute();
+        $Article = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $Article;
+    }
+    
 }
