05b79e84ac5eb87ced45d40ae66858e630629178
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:36:31 +0000

    Ajout de la méthode getLike pour vérifier si un article est aimé

--- app/controller/favoriat.php
+++ app/controller/favoriat.php
@@ -9,4 +9,14 @@ class Favorite {
         $db = new Database();
         $this->connect = $db->getDatabase();
     }
+    public function getLike($idA, $idU) {
+        $sql = 'SELECT * FROM favorite WHERE article_id = :id_article AND client_id = :id_User';
+        $stmt = $this->connect->prepare($sql);
+        $stmt->bindParam(':id_article', $idA);
+        $stmt->bindParam(':id_User', $idU);
+        $stmt->execute();
+        $likes = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $likes;
+    }
+
 }
