f67c04859fae09996646c19636d88574ce887337
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:56:50 +0000

    Fetch Themes and Handle Theme Deletion

--- app/views/themeAdmin.php
+++ app/views/themeAdmin.php
@@ -4,4 +4,11 @@ $userName = $_SESSION['user_name'];
 require __DIR__ . "/../controller/themes.php";
 
 $themes = new themes();
+$result = $themes->getTemes();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['supprimer'])) {
+    $id = intval($_POST['supprimerV']);
+    $themes->DeletTemes($id);
+}
 ?>
+
