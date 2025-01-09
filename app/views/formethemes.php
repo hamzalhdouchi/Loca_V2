8b45b81c9ead3fd984fa6c13344467896f818cbb
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:42:56 +0000

    Vérification du formulaire et traitement des données

--- app/views/formethemes.php
+++ app/views/formethemes.php
@@ -3,6 +3,12 @@ require __DIR__."/../controller/themes.php";
 $idT = $_GET['id_M'];
 $themes = new themes();
 $result = $themes->ModiferTheme($idT);
+if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['ModifierTheme'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $description = htmlspecialchars($_POST['description']);
+    $date = htmlspecialchars($_POST['created_date']);
+    $idT = intval($_POST['id_T']);
+    $themes->Modifer($idT, $name, $description, $date);
+}
 ?>
 
-?>
