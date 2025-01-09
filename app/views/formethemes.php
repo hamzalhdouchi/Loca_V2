58157a70a501b133f38bf4abe8c79685ae46c0c3
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:43:28 +0000

    Début de la structure HTML et métadonnées

--- app/views/formethemes.php
+++ app/views/formethemes.php
@@ -10,5 +10,18 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['ModifierTheme'])) {
     $idT = intval($_POST['id_T']);
     $themes->Modifer($idT, $name, $description, $date);
 }
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>Modern Admin Dashboard</title>
+    <link rel="stylesheet" href="./assets/css/css/dach.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+</head>
 
