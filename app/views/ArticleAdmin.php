271c7a9916d0ba7d0b00387fe3c3d10880089e7e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:58:24 +0000

    Add sidebar with navigation links

--- app/views/ArticleAdmin.php
+++ app/views/ArticleAdmin.php
@@ -25,5 +25,31 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['status'])) {
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
+<body>
+    <input type="checkbox" id="menu-toggle">
+    <div class="sidebar">
+        <div class="side-header">
+            <h3>M<span>odern</span></h3>
+        </div>
+        <div class="side-content">
+            <div class="profile">
+                <div class="profile-img bg-img" style="background-image: url(img/3.jpeg)"></div>
+                <h4><?= $userName ?></h4>
+                <small>Director</small>
+            </div>
+            <div class="side-menu">
+                <ul>
+                    <li><a href="./Dach.php"><span class="las la-user-alt"></span><small>users</small></a></li>
+                    <li><a href="./categorei.php"><span class="las la-th-large"></span><small>categorei</small></a></li>
+                    <li><a href="./vehcule.php"><span class="las la-car"></span><small>véhcule</small></a></li>
+                    <li><a href="./reservationAdmin.php"><span class="las la-clipboard-check"></span><small>reservation</small></a></li>
+                    <li><a href="./evaluations.php"><span class="las la-star"></span><small>Avis</small></a></li>
+                    <li><a href="./themeAdmin.php"><span class="las la-tasks"></span><small>Theme</small></a></li>
+                    <li><a href="./ArticleAdmin.php" class="active"><span class="las la-newspaper"></span><small>Article</small></a></li>
+                    <li><a href="./tagAdmiun.php"><span class="las la-tag"></span><small>Tag</small></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
