1e4b3aedfc8a04d64f85789e2d3a1fdfbdd143cf
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:07:38 +0000

    Récupération des données de l'article et des commentaires

--- app/views/articleditels.php
+++ app/views/articleditels.php
@@ -3,3 +3,14 @@ require __DIR__ . "/../controller/Article.php";
 require __DIR__."/../controller/comment.php";
 require __DIR__."/../controller/favorait.php";
 session_start();
+
+$idU = $_SESSION['user_id'];
+$coment = new comment();
+
+$idA = $_GET['id'];
+
+$comment = $coment->getComment($idA);
+
+$article = new Article();
+$articles = $article->getArticleSpiciale($idA);
+$SpicialArticle = $articles[0];
