0b375ca198faaf3f4dbbae9f439d97a939b44539
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:30:34 +0000

    Finalisation de la classe

--- app/controller/themes.php
+++ app/controller/themes.php
@@ -15,60 +15,59 @@ class themes{
         $sql = "SELECT * FROM theme";
         $stmt = $this->connect->prepare($sql);
         $stmt->execute();
-    
+
         $themes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $themes;
     }
-    
+
     public function DeletTemes($id){
         $sql = "DELETE FROM theme WHERE id = :id";
-    
+
         $stmt = $this->connect->prepare($sql);
         $stmt->bindParam(':id',$id);
         $stmt->execute();
     }
-    
-
-    public function AjouterTheme($idCont, $postdata){
-        $sql = "INSERT INTO theme(name, description, created_Date) VALUES (:name,:description,:date)";
+     public function AjouterTheme($idCont, $postdata){
+        $sql = "INSERT INTO theme(name, description,created_Date) VALUES (:name,:description,:date)";
     
         for ($i = 0; $i <= $idCont; $i++) {
-    
+
             $name = trim($postdata["name_$i"]);
             $description = trim($postdata["description_$i"]);
             $date = trim($postdata["created_date_$i"]);
     
+
             $stmt = $this->connect->prepare($sql);
-    
+
             $stmt->bindParam(':name', $name, PDO::PARAM_STR);
             $stmt->bindParam(':description', $description, PDO::PARAM_STR);
             $stmt->bindParam(':date', $date, PDO::PARAM_STR);
             if ($stmt->execute()) {
-                echo 'Theme ajouté avec succès';
-            } else {
-                echo 'Erreur lors de l\'ajout du thème';
+                echo 'fjfkffkjdfjfjfjfjdjkjjdfdfjjdfjdfjdf';
+            }else {
+                echo 'rf,f';
             }
+            
+        
         }
-    }
-    
+     }
     public function ModiferTheme($id){
         $sql = "SELECT * FROM theme WHERE id = :id";
         $stmt = $this->connect->prepare($sql);
         $stmt->bindParam(':id',$id);
-        $stmt->execute();
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+         $stmt->execute();
+         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    public function Modifer($id,$name,$description,$date){
+            $sql = "UPDATE theme SET name = :name , description = :description , created_date = :date   WHERE id = :id";
     
-    public function Modifer($id, $name, $description, $date){
-        $sql = "UPDATE theme SET name = :name , description = :description , created_date = :date WHERE id = :id";
-    
-        $stmt = $this->connect->prepare($sql);
-        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
-        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
-        $stmt->bindParam(':date', $date, PDO::PARAM_STR); 
-        $stmt->execute();
-    }
-    
+            $stmt = $this->connect->prepare($sql);
+            $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+            $stmt->bindParam(':date', $date, PDO::PARAM_STR); 
+            $stmt->execute();
+        }   
 }
