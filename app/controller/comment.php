1b58e0684f72152f57d29aae4c3989ee78ed57ef
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 10:19:13 +0000

    Ajout de la méthode getComment pour récupérer les commentaires d'un article

--- app/controller/comment.php
+++ app/controller/comment.php
@@ -8,4 +8,20 @@ class comment{
         $db = new Database();
         $this->connect = $db->getdatabase();
     }
+
+    public function getComment($id){
+        $sql = " 
+            SELECT * 
+            FROM comment 
+            JOIN utilisateurs 
+            ON comment.author_id = utilisateurs.id 
+            WHERE article_id = :id;";
+    
+        $stmt = $this->connect->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $comment = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $comment;
+    }
+    
 }
