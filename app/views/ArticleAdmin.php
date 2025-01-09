a3487fcd3fb1f68aed0017bb7f1b131d51b83cf7
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:59:21 +0000

    Add main header with notifications and user menu

--- app/views/ArticleAdmin.php
+++ app/views/ArticleAdmin.php
@@ -52,4 +52,38 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['status'])) {
         </div>
     </div>
 
+    <div class="main-content">
+        <header>
+            <div class="header-content">
+                <label for="menu-toggle"><span class="las la-bars"></span></label>
+                <div class="header-menu">
+                    <label for=""><span class="las la-search"></span></label>
+                    <div class="notify-icon"><span class="las la-envelope"></span><span class="notify">4</span></div>
+                    <div class="notify-icon"><span class="las la-bell"></span><span class="notify">3</span></div>
+                    <div class="user">
+                        <div class="bg-img" style="background-image: url(img/1.jpeg)"></div>
+                        <span class="las la-power-off"></span>
+                        <span><a href="./register.php">Logout</a></span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    <div class="main-content">
+        <header>
+            <div class="header-content">
+                <label for="menu-toggle"><span class="las la-bars"></span></label>
+                <div class="header-menu">
+                    <label for=""><span class="las la-search"></span></label>
+                    <div class="notify-icon"><span class="las la-envelope"></span><span class="notify">4</span></div>
+                    <div class="notify-icon"><span class="las la-bell"></span><span class="notify">3</span></div>
+                    <div class="user">
+                        <div class="bg-img" style="background-image: url(img/1.jpeg)"></div>
+                        <span class="las la-power-off"></span>
+                        <span><a href="./register.php">Logout</a></span>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
 
