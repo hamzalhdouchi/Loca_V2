45bf56d171ee3b213ec49004aeea483e03725651
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-09 09:57:56 +0000

    Add basic HTML structure with doctype and head

--- app/views/ArticleAdmin.php
+++ app/views/ArticleAdmin.php
@@ -3,7 +3,6 @@ session_start();
 $userName = $_SESSION['user_name'];
 require __DIR__ . "/../controller/Article.php";
 
-<?php
 $themes = new Article();
 
 $result = $themes->getArticle();
@@ -15,4 +14,16 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['status'])) {
 }
 ?>
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>Modern Admin Dashboard</title>
+    <link rel="stylesheet" href="./assets/css/css/dach.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+</head>
+
+
